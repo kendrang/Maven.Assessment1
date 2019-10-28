9fa64cc5e8e596052aef56de8c59694f200fa2bb
kendrang <kendrang@users.noreply.github.com>
2019-10-28 09:44:31 -0400

    did a few more

--- src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
@@ -9,7 +9,8 @@ public class BasicStringUtils {
      * @return string with identical content, and the first character capitalized
      */
     public static String camelCase(String str) {
-        return null;
+
+        return Character.toUpperCase(str.charAt(0))+ str.substring(1);
     }
 
     /**
@@ -17,7 +18,12 @@ public class BasicStringUtils {
      * @return string with identical contents, in the reverse order
      */
     public static String reverse(String str) {
-        return null;
+        String result = "" ;
+        for (int i =str.length()-1; i>=0;i--){
+            result = result + str.charAt(i);
+        }
+
+        return result;
     }
 
     /**
@@ -25,7 +31,9 @@ public class BasicStringUtils {
      * @return string with identical contents, in reverse order, with first character capitalized
      */
     public static String reverseThenCamelCase(String str) {
-        return null;
+
+        return camelCase(reverse(str));
+
     }
 
 
@@ -34,7 +42,16 @@ public class BasicStringUtils {
      * @return string with identical contents excluding first and last character
      */
     public static String removeFirstAndLastCharacter(String str) {
-        return null;
+
+            String result = "" ;
+            for (int i=0 ; i< str.length() ; i++){
+                if (str.charAt(i) != str.charAt(0) && str.charAt(i) != str.charAt(str.length()-1)){
+                    result += str.charAt(i);
+
+                }
+
+            }
+            return result;
     }
 
     /**
